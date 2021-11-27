0e9f13b893a39896791ce32ae28f85620a15510b
ReiKohaku <hbsjzjwj@163.com>
2021-11-27 18:34:25 +0800

    fix: ffxiv.time: Fix a display bug

--- src/lib/util/eorzeaTime.ts
+++ src/lib/util/eorzeaTime.ts
@@ -12,11 +12,11 @@ export class EorzeaTime {
         const timeNum = !time ? new Date().getTime() : (typeof time === "number") ? time : time.getTime();
         const eorzeaTimeNum = Math.floor(timeNum / 1000 * 3600 / 175);
         this.year = Math.floor(eorzeaTimeNum / TimeUnit.YEAR) + 1;
-        this.month = Math.floor(eorzeaTimeNum / TimeUnit.MONTH % 12) + 1;
-        this.day = Math.floor(eorzeaTimeNum / TimeUnit.DAY % 32) + 1;
-        this.hour = Math.floor(eorzeaTimeNum / TimeUnit.HOUR % 24);
-        this.minute = Math.floor(eorzeaTimeNum / TimeUnit.MINUTE % 60);
-        this.second = Math.floor(eorzeaTimeNum / TimeUnit.SECOND % 60);
+        this.month = Math.floor((eorzeaTimeNum / TimeUnit.MONTH) % 12) + 1;
+        this.day = Math.floor((eorzeaTimeNum / TimeUnit.DAY) % 32) + 1;
+        this.hour = Math.floor((eorzeaTimeNum / TimeUnit.HOUR) % 24);
+        this.minute = Math.floor((eorzeaTimeNum / TimeUnit.MINUTE) % 60);
+        this.second = Math.floor((eorzeaTimeNum / TimeUnit.SECOND) % 60);
     }
 
     public toTimeString(): string {
--- src/lib/util/format.ts
+++ src/lib/util/format.ts
@@ -1,5 +1,5 @@
 export function prefixNum(num: number, length: number = 2): string {
     let str = num.toString();
-    for (let i = str.length; i < length; i++) str += "0";
+    for (let i = str.length; i < length; i++) str = "0" + str;
     return str;
 }
