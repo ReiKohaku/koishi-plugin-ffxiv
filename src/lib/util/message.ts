acea402bb7d2a0288e2a72aee010baebc372266f
ReiKohaku <hbsjzjwj@163.com>
2023-03-12 12:42:52 +0800

    fix: message quote

--- src/lib/util/message.ts
+++ src/lib/util/message.ts
@@ -1,15 +1,13 @@
-import {segment, Session} from "koishi";
+import {h, Session} from "koishi";
 
 function _wrapReply (messageId: string, content: string)
 function _wrapReply (session: Session, content: string)
 function _wrapReply (message: string | Session, content: string) {
-    const r = (id: string) => segment("quote", { id }) + " " + content;
+    const r = (id: string) => h.quote(id) + " " + content;
     if (typeof message === "string") return r(message)
     else if (message.platform === "onebot" && message.subtype === "group") {
         // 腾讯定制回复 解决缩略消息吞昵称的问题 真有你的腾讯
-        return segment("quote", { id: message.messageId }) +
-            segment("at", { id: message.userId }) +
-            content;
+        return `${h.quote(message.messageId)}${h.at(message.userId)} ${content}`;
     }
     else if (message.subtype === "private") return content;
     return r(message.messageId);
